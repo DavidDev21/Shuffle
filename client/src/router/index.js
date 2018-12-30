f583c38808bcd9d2cbfea193b7e9189a0d81a2ba
DavidDev21 <davidzheng54@gmail.com>
2018-12-30 10:38:50 -0500

    Added NotFound 404 page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import SignIn from '@/components/Pages/SignIn'
+import NotFound from '@/components/Pages/NotFound'
 
 Vue.use(Router)
 // change the router mode to 'history' to remove the # from the URL
@@ -16,6 +17,10 @@ export default new Router({
       path: '/SignIn',
       name: 'SignIn',
       component: SignIn
+    },
+    {
+      path: '*',
+      component: NotFound
     }
   ]
 })
