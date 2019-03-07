3d1c0283356552adf5fb67a18c68c866d9dd6de4
DavidDev21 <davidzheng54@gmail.com>
2019-03-07 17:01:37 -0500

    fixed dashboard buttons and routes

--- client2/src/views/DashBoard.vue
+++ client2/src/views/DashBoard.vue
@@ -4,14 +4,14 @@
         <h3>Welcome to your dashboard, {{userType}}</h3>
         <div class='row'>
             <div class='col-12'>
-                <SelectionButton btnText='Apply To Job' to='/applyjob'></SelectionButton>
-                <SelectionButton></SelectionButton>
+                <SelectionButton class="btn-sq-big btn-light btn ml-3" btnText='Apply To Job' to='/apply-job'></SelectionButton>
+                <SelectionButton class="btn-sq-big btn-light btn ml-3"></SelectionButton>
             </div>
         </div>
         <div class='row mt-2'>
             <div class='col-12'>
-                <SelectionButton></SelectionButton>
-                <SelectionButton></SelectionButton>
+                <SelectionButton class="btn-sq-big btn-light btn ml-3"></SelectionButton>
+                <SelectionButton class="btn-sq-big btn-light btn ml-3"></SelectionButton>
             </div>
         </div>
     </div>
@@ -37,4 +37,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+/* Desktops and laptops ----------- medium */
+@media screen and (min-width: 600px) {
+  .btn-sq-big{
+      padding: 0;
+      width: 300px !important;
+      height: 300px !important;
+  }
+};
+
+/* Large screens ----------- large */
+@media (min-width : 900px) {
+  .btn-sq-big{
+      padding: 0;
+      width: 450px !important;
+      height: 450px !important;
+  }
+};
 </style>
