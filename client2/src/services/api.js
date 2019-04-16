18e596d2cd1db8a890883b0604b573ee8fdab43b
DavidDev21 <davidzheng54@gmail.com>
2019-04-16 13:22:53 -0400

    axios test 3

--- client2/src/services/api.js
+++ client2/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-alert(process.env);
+alert(process.env.API_ENDPOINT);
 
 export default () => axios.create({
   baseURL: process.env.API_ENDPOINT || 'http://localhost:5000/',
