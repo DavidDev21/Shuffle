c9edddc0ac6888443298d008b22a76a4a24aa84a
DavidDev21 <davidzheng54@gmail.com>
2019-04-16 13:29:07 -0400

    backend api test

--- client2/src/services/api.js
+++ client2/src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-alert(process.env.VUE_APP_API_ENDPOINT);
 
 export default () => axios.create({
-  baseURL: process.env.VUE_APP_API_ENDPOINT || 'http://localhost:5000/',
+  baseURL: "https://shuffleproject.herokuapp.com/",
 });
