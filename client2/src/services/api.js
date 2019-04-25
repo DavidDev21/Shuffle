cb03d50f81cba1a5279de43a041be54ab73a46a5
DavidDev21 <davidzheng54@gmail.com>
2019-04-25 15:32:22 -0400

    axios

--- client2/src/services/api.js
+++ client2/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default () => axios.create({
-  // baseURL: "https://shuffleproject.herokuapp.com/",
-    baseURL: "http://localhost:5000/",
+  baseURL: "https://shuffleproject.herokuapp.com/",
+    // baseURL: "http://localhost:5000/",
 });
